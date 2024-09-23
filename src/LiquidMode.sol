215ae26d1bc6c1718a62f62dd4a823e32c8056c5
sunosuporno <sarkarsuporno36@gmail.com>
2024-09-23 20:27:02 +0530

    Change version of CryptoAlgebra contracts

--- src/LiquidMode.sol
+++ src/LiquidMode.sol
@@ -1,112 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {IxRenzoDeposit} from "./interfaces/IxRenzoDeposit.sol";
 import {IRSETHPoolV2} from "./interfaces/IRSETHPoolV2.sol";
+import "@cryptoalgebra/integral-core/contracts/interfaces/IAlgebraPool.sol";
+import "@cryptoalgebra/integral-core/contracts/libraries/TickMath.sol";
 
-contract VaultStrategy is
-    Initializable,
-    ERC4626Upgradeable,
-    Ownable2StepUpgradeable,
-    AccessControlUpgradeable,
-    ReentrancyGuardUpgradeable,
-    UUPSUpgradeable,
-    PausableUpgradeable
-{
+import "@cryptoalgebra/integral-periphery/contracts/libraries/TransferHelper.sol";
+import "@cryptoalgebra/integral-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@cryptoalgebra/integral-periphery/contracts/base/LiquidityManagement.sol";
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract VaultStrategy is ERC4626, Ownable, AccessControl, ReentrancyGuard, Pausable, LiquidityManagement {
     using Math for uint256;
     using SafeERC20 for IERC20;
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    IxRenzoDeposit public xRenzoDeposit;
-    IRSETHPoolV2 public rSETHPoolV2;
+    IxRenzoDeposit public immutable xRenzoDeposit;
+    IRSETHPoolV2 public immutable rSETHPoolV2;
+
+    INonfungiblePositionManager public immutable nonfungiblePositionManager;
+    address public immutable EZETH;
+    address public immutable WRSETH;
+    uint24 public immutable KIM_FEE;
+
+    uint256 public kimTokenId;
+    uint128 public kimLiquidity;
 
     bytes32 public constant REBALANCER_ROLE = keccak256("REBALANCER_ROLE");
 
-    // Add strategist address and fee percentage
     address public strategist;
     uint256 public accumulatedStrategistFee;
     uint256 public constant STRATEGIST_FEE_PERCENTAGE = 2000; // 20% with 2 decimal places
 
-    // Add this state variable
     uint256 public accumulatedDeposits;
     uint256 public _totalAccountedAssets;
 
     event StrategistFeeClaimed(uint256 claimedAmount, uint256 remainingFees);
 
-    // event PositionRebalanced(
-    //     uint256 additionalAmountNeeded,
-    //     uint256 amountFreedUp,
-    //     uint256 lpTokensBurned,
-    //     uint256 usdcReceived,
-    //     uint256 aeroReceived,
-    //     uint256 cbEthRepaid
-    // );
-
-    // event HarvestReport(
-    //     uint256 totalProfit,
-    //     uint256 netProfit,
-    //     uint256 strategistFee,
-    //     uint256 aerodromeRewards,
-    //     int256 aaveNetGain,
-    //     uint256 claimedAero,
-    //     uint256 claimedUsdc
-    // );
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(
+    constructor(
         IERC20 asset_,
-        uint256 _initialDeposit,
-        address initialOwner,
         string memory name_,
         string memory symbol_,
+        address initialOwner,
         address _strategist,
         address _xRenzoDeposit,
-        address _rSETHPoolV2
-    ) public initializer {
-        __ERC4626_init(asset_);
-        __ERC20_init(name_, symbol_);
-        __Ownable_init(initialOwner);
-        __AccessControl_init();
-        __ReentrancyGuard_init();
-        __UUPSUpgradeable_init();
-        __Pausable_init();
-
+        address _rSETHPoolV2,
+        INonfungiblePositionManager _nonfungiblePositionManager,
+        address _factory,
+        address _WNativeToken,
+        address _poolDeployer,
+        address _EZETH,
+        address _WRSETH,
+        uint24 _kimFee
+    )
+        ERC4626(asset_)
+        ERC20(name_, symbol_)
+        Ownable(initialOwner)
+        LiquidityManagement(_factory, _WNativeToken, _poolDeployer)
+    {
         _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
         _grantRole(REBALANCER_ROLE, initialOwner);
 
         strategist = _strategist;
         xRenzoDeposit = IxRenzoDeposit(_xRenzoDeposit);
         rSETHPoolV2 = IRSETHPoolV2(_rSETHPoolV2);
+
+        nonfungiblePositionManager = _nonfungiblePositionManager;
+        EZETH = _EZETH;
+        WRSETH = _WRSETH;
+        KIM_FEE = _kimFee;
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-    // New internal function that includes priceUpdate
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
         address assetAddress = asset();
-        // Transfer the assets from the caller to this contract
         SafeERC20.safeTransferFrom(IERC20(assetAddress), caller, address(this), assets);
 
-        // Mint shares to the receiver
         _mint(receiver, shares);
 
-        // Accumulate deposits instead of investing
         accumulatedDeposits += assets;
 
         emit Deposit(caller, receiver, assets, shares);
@@ -114,7 +95,43 @@ contract VaultStrategy is
 
     function _investFunds(uint256 amount, address assetAddress) internal {
         uint256 receivedEzETH = xRenzoDeposit.depositETH{value: amount / 2}(0, block.timestamp);
-        rSETHPoolV2.deposit{value: amount / 2}("string");
+        rSETHPoolV2.deposit{value: amount / 2}("");
+
+        _createKIMPosition(receivedEzETH, receivedRSETH);
+    }
+
+    function _createKIMPosition(uint256 amount0, uint256 amount1) internal {
+        IERC20(EZETH).approve(address(nonfungiblePositionManager), amount0);
+        IERC20(RSETH).approve(address(nonfungiblePositionManager), amount1);
+
+        INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
+            token0: EZETH,
+            token1: RSETH,
+            tickLower: TickMath.MIN_TICK,
+            tickUpper: TickMath.MAX_TICK,
+            amount0Desired: amount0,
+            amount1Desired: amount1,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+
+        (uint256 tokenId, uint128 liquidity,,) = nonfungiblePositionManager.mint(params);
+
+        kimTokenId = tokenId;
+        kimLiquidity = liquidity;
+    }
+
+    function collectKIMFees() internal returns (uint256 amount0, uint256 amount1) {
+        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
+            tokenId: kimTokenId,
+            recipient: address(this),
+            amount0Max: type(uint128).max,
+            amount1Max: type(uint128).max
+        });
+
+        return nonfungiblePositionManager.collect(params);
     }
 
     function _withdrawFunds(uint256 amount) internal nonReentrant {}
@@ -129,22 +146,18 @@ contract VaultStrategy is
         return _totalAccountedAssets;
     }
 
-    // Function to grant the rebalancer role
     function grantRebalancerRole(address account) external onlyOwner {
         grantRole(REBALANCER_ROLE, account);
     }
 
-    // Function to revoke the rebalancer role
     function revokeRebalancerRole(address account) external onlyOwner {
         revokeRole(REBALANCER_ROLE, account);
     }
 
-    // Function to set the strategist address
     function setStrategist(address _strategist) external onlyOwner {
         strategist = _strategist;
     }
 
-    // Override the deposit function to include the nonReentrant modifier
     function deposit(uint256 assets, address receiver)
         public
         virtual
@@ -156,7 +169,6 @@ contract VaultStrategy is
         return super.deposit(assets, receiver);
     }
 
-    // Override the mint function to include the nonReentrant modifier
     function mint(uint256 shares, address receiver)
         public
         virtual
@@ -168,7 +180,6 @@ contract VaultStrategy is
         return super.mint(shares, receiver);
     }
 
-    // Override the withdraw function to include the nonReentrant modifier
     function withdraw(uint256 assets, address receiver, address owner)
         public
         virtual
@@ -180,7 +191,6 @@ contract VaultStrategy is
         return super.withdraw(assets, receiver, owner);
     }
 
-    // Override the redeem function to include the nonReentrant modifier
     function redeem(uint256 shares, address receiver, address owner)
         public
         virtual
@@ -192,7 +202,6 @@ contract VaultStrategy is
         return super.redeem(shares, receiver, owner);
     }
 
-    // Add pause and unpause functions
     function pause() external onlyOwner {
         _pause();
     }
@@ -201,9 +210,6 @@ contract VaultStrategy is
         _unpause();
     }
 
-    /**
-     * @dev Override the internal _withdraw function from ERC4626
-     */
     function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
         internal
         virtual
@@ -220,5 +226,9 @@ contract VaultStrategy is
         SafeERC20.safeTransfer(IERC20(asset()), receiver, assets);
 
         emit Withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function WNativeToken() public view returns (address) {
+        return WNativeToken;
     }
 }
